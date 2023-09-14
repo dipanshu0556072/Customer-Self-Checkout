50dfcd38b0074dd19a46bf38b908760ad32a4933
dipanshu0556072 <51964503+dipanshu0556072@users.noreply.github.com>
2023-09-14 13:08:19 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import logo from './logo.svg';
+import SlideAnimation  from './Popup';
 import './App.css';
-import Header from './Header/Header';
-import CustomerDetail from './Header/CustomerDetail';
-import { BrowserRouter,Router, Routes, Route } from "react-router-dom";
-import Footer from './Footer/Footer';
-import Welcome from './Welcome/Welcome';
-import MobileSign from './Mobile/MobileSign';
-import PaymentTile from './PaymentTiles/PayementTile';
-import Upi from './UPI/Upi';
-import Feedback from './Feedback/Feedback';
-import DebitPay from './DebitPay/DebitPay';
-import Cart from './Cart/Cart';
-export default function App() {
-  return (
-   <>
-    
-       <Cart/>
 
-    </> 
+function App() {
+  return (
+    <div className="App">
+     <SlideAnimation /> 
+    </div>
   );
 }
+
+export default App;
